468b9fddc5bc11600412bc24f3069e468ec0487d
Luna Seifert <thepuddingdeity@gmail.com>
2023-01-05 19:36:17 -0700

    Added Game route to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import MainHeader from './MainHeader'
+import Game from './Game'
 
 function App() {
   return (
     <div className="App">
       <MainHeader />
+      <Switch>
+        <Route exact path="/game">
+          <Game />
+        </Route>
+      </Switch>
     </div>
   );
 }
