25a1350ff19172ab7dc314e5dd7655f02326b1f1
Luna Seifert <thepuddingdeity@gmail.com>
2023-01-09 17:06:46 -0700

    Added a color picker to the canvas and refined where things are drawn.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,21 +1,37 @@
 import React, {useState} from 'react'
+import {SketchPicker} from 'react-color'
 
 function Canvas({canvasRef}){
     const [fillStyle, setFillStyle] = useState('#000')
+    const [mousePos, setMousePos] = useState({
+        left: 0,
+        top: 0
+    })
+
+    function handleMouse(e){
+        setMousePos({
+            left: e.pageX- 8,
+            top: e.pageY - 62
+        })
+    }
 
     function updateColor(color){
-        setFillStyle(`#${color}`)
+        setFillStyle(color)
     }
 
     function handleClick(e){
         const ctx = canvasRef.current.getContext('2d')
-        ctx.fillStyle = fillStyle
-        ctx.fillRect(e.clientX, e.clientY, 10, 10)
+        ctx.fillStyle = fillStyle.hex
+
+        ctx.fillRect(mousePos.left - 10, mousePos.top - 10, 10, 10)
     }
+
 
     return(
         <div>
-            <canvas ref={canvasRef} className="drawing" height='150px' width='150px' onClick={handleClick}></canvas>
+            <canvas onMouseMove={handleMouse} ref={canvasRef} className="drawing" height='200px' width='200px' onClick={handleClick}></canvas>
+            <h1>X: {mousePos.left} Y: {mousePos.top}</h1>
+            <SketchPicker className="color-picker" color={fillStyle.hex} onChange={updateColor}/>
         </div>
     )
 }
