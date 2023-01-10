b2badd5eb1ec7d1ccfe2f66f54c24aaa31627a05
Luna Seifert <thepuddingdeity@gmail.com>
2023-01-09 17:59:02 -0700

    Styled the Component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react'
 import {SketchPicker} from 'react-color'
 
 function Canvas({canvasRef}){
-    const [fillStyle, setFillStyle] = useState('#000')
+    const [fillStyle, setFillStyle] = useState({"hex": "#000"})
     const [mousePos, setMousePos] = useState({
         left: 0,
         top: 0
     })
+    const [tool, setTool] = useState('pencil')
 
     function handleMouse(e){
         setMousePos({
@@ -19,20 +20,38 @@ function Canvas({canvasRef}){
         setFillStyle(color)
     }
 
-    function handleClick(e){
+    function handleClick(){
         const ctx = canvasRef.current.getContext('2d')
-        ctx.fillStyle = fillStyle.hex
 
-        ctx.fillRect(mousePos.left - 10, mousePos.top - 10, 10, 10)
+        if (tool === 'pencil'){
+            ctx.fillStyle = fillStyle.hex
+            ctx.fillRect(mousePos.left - 10, mousePos.top - 10, 10, 10)
+        }
+        else{
+            ctx.clearRect(mousePos.left - 10, mousePos.top - 10, 10, 10)
+        }
     }
 
+    function clearCanvas(){
+        canvasRef.current.getContext('2d').clearRect(0, 0, 200, 200)
+    }
+
+    function handleTool(tool){
+        setTool(tool)
+    }
 
     return(
-        <div>
-            <canvas onMouseMove={handleMouse} ref={canvasRef} className="drawing" height='200px' width='200px' onClick={handleClick}></canvas>
-            <h1>X: {mousePos.left} Y: {mousePos.top}</h1>
-            <SketchPicker className="color-picker" color={fillStyle.hex} onChange={updateColor}/>
-        </div>
+        <>
+            <div className='drawing'>
+                <canvas onMouseMove={handleMouse} ref={canvasRef} className="canvas" height='200px' width='200px' onClick={handleClick}></canvas>
+                <SketchPicker className="color-picker" color={fillStyle.hex} onChange={updateColor}/>
+                <div className='canvas-buttons'>
+                    <button onClick={()=>handleTool('pencil')}>✏️</button>
+                    <button onClick={()=>handleTool('eraser')}>Eraser</button>
+                    <button onClick={clearCanvas}>🗑️</button>
+                </div>
+            </div>
+        </>
     )
 }
 
