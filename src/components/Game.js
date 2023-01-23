488e8d3be3687cbd56a86f93a437c7e3117c59b6
Luna Seifert <thepuddingdeity@gmail.com>
2023-01-23 15:13:42 -0600

    Added part of Mob's editing capability and music.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+const nyan = [require('../music/angel-eyes.mp3'), require('../music/man-is-he-mega.mp3'), require('../music/dnx.mp3'), require('../music/leave-the-world-tonight.mp3'), require('../music/legends.mp3')]
+
+
 function Game(){
+    console.log(nyan)
+
     return(
         <div id='game'>
+            <audio controls>
+                <source src={nyan[Math.floor(Math.random() * nyan.length)]} type="audio/mpeg"></source>
+            </audio>
         </div>
     )
 }
