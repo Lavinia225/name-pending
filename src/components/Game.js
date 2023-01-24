af7e2401ff92e87eaafd2b999316bacd57d3771c
Luna Seifert <thepuddingdeity@gmail.com>
2023-01-24 01:00:28 -0600

    Player movement now exists

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,25 +7,32 @@ function Game({players, mobs}){
     const [playerPos, setPlayerPos] = useState({
         position: 'absolute',
         left: `400px`,
-        top: `400px`
+        top: `390px`
     })
 
     function handleClick(){
-        audioRef.current.volume = 0.01
+        audioRef.current.volume = 0.01 //Temporarily not .33
         audioRef.current.play()
     }
 
     function handleKey(e){
-        if(e.key === 'w'){
-            setPlayerPos({...playerPos, left: `${parseInt(playerPos.left) + 10}px`})
+        if(e.key === 'd'){
+            if(parseInt(playerPos.left) <= 580){
+                setPlayerPos({...playerPos, left: `${parseInt(playerPos.left) + 10}px`})
+            }
         }
-        console.log(playerPos)
+
+        if(e.key === 'a'){
+            if(parseInt(playerPos.left) >= 10){
+                setPlayerPos({...playerPos, left: `${parseInt(playerPos.left) - 10}px`})
+            }
+        }
     }
 
     return(
         <div id='game' onClick={handleClick} tabIndex={0} onKeyDown={handleKey}>
             <Audio audioRef={audioRef}/>
-            <Entity playerPos={playerPos} entity={players[0]} isMob={false} isGame={true}/> {/*Entity, isMob, isGame are the props */}
+            <Entity playerPos={playerPos} entity={players[2]} isMob={false} isGame={true}/> {/*Entity, isMob, isGame are the props */}
         </div>
     )
 }
