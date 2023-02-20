b0237e61dc107e64fcd0814dddf0d533da61f430
Luna Seifert <thepuddingdeity@gmail.com>
2023-02-19 18:52:45 -0600

    Bullet now exits with hitting the mob properly. Except.... when the console is closed.

--- src/components/Bullet.js
+++ src/components/Bullet.js
@@ -4,33 +4,48 @@ import bulletSprite from '../images/bullet.png'
 function Bullet({playerPos, exit}){
     const [bulletPos, setBulletPos] = useState({
         left: `${parseInt(playerPos.left) + 50}px`,
-        top: '425px'
+        right: `${parseInt(playerPos.left) + 54}px`,
+        top: '425px',
+        bottom: '490px'
     })
     const mob = document.getElementById('mob')
 
     useEffect(()=>{
         const mobHitbox = mob.getBoundingClientRect()
 
-        if(parseInt(bulletPos.top) <= 0){
-            console.warn('Terminated via boundry.')
+            //bottom of bullet must be less than y or 'top' of mob. Implement bottom.
+            //Right of bullet must be greater than left of mob.
+            //Left of bullet must be less than right of mob.
+        if(parseInt(bulletPos.bottom) <= mobHitbox.top && parseInt(bulletPos.right) > mobHitbox.left && parseInt(bulletPos.left) < mobHitbox.right){
+            console.warn(`TERMINATED`)
+        
+            /*console.log(mobHitbox)
+            console.log('Bullet top', parseInt(bulletPos.top))
+            console.log('Bullet left', parseInt(bulletPos.left))
+            console.log('Mob y', mobHitbox.y)
+            console.log('Mob left', mobHitbox.left)
+            console.log('Mob right', mobHitbox.right)
+            console.table(document.getElementById('bullet').getBoundingClientRect())*/
             exit()
         }
-        
-        if(parseInt(bulletPos.top) <= mobHitbox.y && parseInt(bulletPos.left) > mobHitbox.left && parseInt(bulletPos.left) < mobHitbox.right){
-            console.warn(`TERMINATED`)
 
-           /*console.log(parseInt(bulletPos.left) < mobHitbox.right)
-            console.log('Bullet:')
-            console.table(document.getElementById('bullet').getBoundingClientRect())
-            console.log('MobBox:')
+        if(parseInt(bulletPos.top) <= 0){
+            console.warn('Terminated via boundry.')
+            console.log('bullet')
+            console.table(bulletPos)
+            console.log('\nmob')
             console.table(mobHitbox)
-            console.log(parseInt(bulletPos.top) <= mobHitbox.y)*/
             exit()
         }
 
         const time = setInterval(()=>{
             setBulletPos(()=>{
-                return {left: bulletPos.left, top: `${parseInt(bulletPos.top) - 10}px`}})
+                return {
+                    left: bulletPos.left,
+                    right: bulletPos.right,
+                    top: `${parseInt(bulletPos.top) - 10}px`,
+                    bottom: `${parseInt(bulletPos.bottom) - 10}px`
+                }})
             }, 10)
 
         return function cleanup(){
