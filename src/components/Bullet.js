16723f4b877847936ca84d859b96871bdefb03e5
Luna Seifert <thepuddingdeity@gmail.com>
2023-02-28 15:19:39 -0600

    Hitbox bug finally fixed!

--- src/components/Bullet.js
+++ src/components/Bullet.js
@@ -12,35 +12,18 @@ function Bullet({playerPos, exit}){
 
     useEffect(()=>{
         const mobHitbox = mob.getBoundingClientRect()
+        const bulletHitbox = document.getElementById('bullet').getBoundingClientRect()
 
-            //bottom of bullet must be greater than y or 'top' of mob. Implement bottom.
-            //Right of bullet must be greater than left of mob.
-            //Left of bullet must be less than right of mob.
-            //Top of bullet must be greater than bottom of mob.
-
-        if(parseInt(bulletPos.bottom) >= mobHitbox.top &&
-           parseInt(bulletPos.right) > mobHitbox.left &&
-           parseInt(bulletPos.left) < mobHitbox.right &&
-           parseInt(bulletPos.top) <= mobHitbox.bottom){
+        if(parseInt(bulletHitbox.bottom) >= mobHitbox.top &&
+           parseInt(bulletHitbox.right) > mobHitbox.left &&
+           parseInt(bulletHitbox.left) < mobHitbox.right &&
+           parseInt(bulletHitbox.top) <= mobHitbox.bottom){
             console.warn(`TERMINATED`)
-            /*console.log('parseInt(bulletPos.bottom) >= mobHitbox.top',parseInt(bulletPos.bottom) >= mobHitbox.top)
-            console.log('parseInt(bulletPos.right) > mobHitbox.left', parseInt(bulletPos.right) > mobHitbox.left)
-            console.log('parseInt(bulletPos.left) < mobHitbox.right',parseInt(bulletPos.left) < mobHitbox.right)
-            console.log('parseInt(bulletPos.top) <= mobHitbox.bottom', parseInt(bulletPos.top) <= mobHitbox.bottom)
-            
-            console.log('bullet')
-            console.table(bulletPos)
-            console.log('\nmob')
-            console.table(mobHitbox)*/
             exit(true)
         }
 
         if(parseInt(bulletPos.top) <= 0){
-            /*console.warn('Terminated via boundry.')
-            console.log('bullet')
-            console.table(bulletPos)
-            console.log('\nmob')
-            console.table(mobHitbox)*/
+            console.warn('Terminated via boundry.')
             exit(false)
         }
 
@@ -61,7 +44,7 @@ function Bullet({playerPos, exit}){
 
     return(
         <img src={bulletSprite} id='bullet'
-        style={{position: 'absolute', left: bulletPos.left, top: bulletPos.top}} alt='bullet'></img>
+        style={{left: bulletPos.left, top: bulletPos.top}} alt='bullet'></img>
     )
 }
 
